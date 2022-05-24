6e91ba9391d78e3600b4c55faf82ee959f5e456f
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-24 14:56:26 -0400

    button added

---
+++ client/src/components/homePageBooking/HomePageBooking.jsx
@@ -0,0 +1,9 @@
+import Button from "../../UI/button/Button";
+
+export default function HomePageBooking() {
+  return (
+    <div>
+      <Button>Book Now!</Button>
+    </div>
+  );
+}
--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,5 +1,6 @@
 import Layout from "../../components/layout/Layout";
 import logo from "../../assets/logo/hudson-valley-makeup-logo.png";
+import HomePageBooking from "../../components/homePageBooking/HomePageBooking";
 
 export default function Home() {
   return (
@@ -8,6 +9,10 @@ export default function Home() {
         src={logo}
         alt="hands shaped in a heart with name of company and artist underneath"
       />
+      <p className="font-IBM-Plex-Serif mb-16">
+        "Making beauty more clean by using all natural products"
+      </p>
+      <HomePageBooking />
     </Layout>
   );
 }
