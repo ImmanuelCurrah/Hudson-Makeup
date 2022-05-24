767285028db886ca6ee83c1c585236d5cd10c243
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-24 12:22:49 -0400

    starting to build out the folder structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
