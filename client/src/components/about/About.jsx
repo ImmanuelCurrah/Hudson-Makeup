7fa0633a47cf04128fdd46edda4ac773a24e0755
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-24 15:09:25 -0400

    an about description was added

---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,19 @@
+export default function About() {
+  return (
+    <div>
+      <p className="mt-16 mx-32">
+        Sarah is... Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+        Vero nulla mollitia dignissimos delectus architecto aut eos quas
+        reprehenderit illum voluptas, error facilis perferendis assumenda
+        tenetur officia molestias odit tempora deserunt. Lorem ipsum, dolor sit
+        amet consectetur adipisicing elit. Quae laudantium expedita aperiam
+        error nesciunt ex velit tenetur, sapiente laborum eum commodi assumenda
+        placeat iusto magnam numquam reprehenderit suscipit culpa quisquam.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem, numquam
+        commodi. Nam dicta quos voluptatum sapiente perferendis facere minus non
+        voluptatem, rerum labore voluptas, expedita deserunt natus illo! Quis,
+        quidem.
+      </p>
+    </div>
+  );
+}
--- client/src/screens/home/Home.jsx
+++ client/src/screens/home/Home.jsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/layout/Layout";
 import logo from "../../assets/logo/hudson-valley-makeup-logo.png";
 import HomePageBooking from "../../components/homePageBooking/HomePageBooking";
+import About from "../../components/about/About";
 
 export default function Home() {
   return (
@@ -9,10 +10,11 @@ export default function Home() {
         src={logo}
         alt="hands shaped in a heart with name of company and artist underneath"
       />
-      <p className="font-IBM-Plex-Serif mb-16">
+      <p className="font-IBM-Plex-Serif mb-12">
         "Making beauty more clean by using all natural products"
       </p>
       <HomePageBooking />
+      <About />
     </Layout>
   );
 }
