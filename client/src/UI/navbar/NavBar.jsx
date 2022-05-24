a56732259923c41662b641d2fde7ddc2c80dd861
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-24 14:45:43 -0400

    some transition on the links on homepage

--- client/src/UI/navbar/NavBar.jsx
+++ client/src/UI/navbar/NavBar.jsx
@@ -11,7 +11,10 @@ export default function NavBar() {
     <div className="h-32 bg-white flex flex-row justify-around items-end">
       {navOptions.map((navElement, index) => {
         return (
-          <div key={index} className="cursor-pointer mb-5 font-IBM-Plex-Serif">
+          <div
+            key={index}
+            className="cursor-pointer mb-5 font-IBM-Plex-Serif border:none hover:border-b-2 border-black ease-in-out duration-100"
+          >
             {navElement}
           </div>
         );
