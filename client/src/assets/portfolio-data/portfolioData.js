e0c130f5621fd38a380de9d7dafdd8c9fb139fe1
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-25 12:05:08 -0400

    portofolio is showing images

---
+++ client/src/assets/portfolio-data/portfolioData.js
@@ -0,0 +1,24 @@
+import one from "../pictures/about-me-front-pic.jpeg";
+import two from "../pictures/couple-two.jpeg";
+import three from "../pictures/couple.jpeg";
+import four from "../pictures/hey-smiley-face.jpeg";
+import five from "../pictures/locked-out-lol.jpeg";
+import six from "../pictures/retreat-vibes.jpeg";
+import seven from "../pictures/sarah-on-rock.jpeg";
+import eight from "../pictures/Sarah.jpeg";
+import nine from "../pictures/sunlight.jpeg";
+
+export const pictures = [
+  { picture: one, description: "the artist standing next to a rock" },
+  { picture: two, description: "the artist and her boyfriend selfie" },
+  { picture: three, description: "the artist and her boyfriend selfie" },
+  { picture: four, description: "a selfie of the artist" },
+  {
+    picture: five,
+    description: "a picture of the artist and her boyfriend outside apartment",
+  },
+  { picture: six, description: "a selfie of the artist" },
+  { picture: seven, description: "artist on a rock" },
+  { picture: eight, description: "picture of the artist" },
+  { picture: nine, description: "selfie of the artist" },
+];
--- client/src/screens/portfolio/Portfolio.jsx
+++ client/src/screens/portfolio/Portfolio.jsx
@@ -1,9 +1,22 @@
 import Layout from "../../components/layout/Layout";
+import { pictures } from "../../assets/portfolio-data/portfolioData";
 
 export default function Portfolio() {
   return (
     <Layout>
       <div>Portfolio</div>
+      <div className="grid grid-cols-3 gap-4 m-4">
+        {pictures.map((pic, index) => {
+          return (
+            <img
+              className="h-96"
+              key={index}
+              src={pic.picture}
+              alt={pic.description}
+            />
+          );
+        })}
+      </div>
     </Layout>
   );
 }
