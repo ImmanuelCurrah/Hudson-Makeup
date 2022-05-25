71d23432cb177d368e11b0751adc170c3aea4cf5
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-25 11:41:58 -0400

    about me page is done for now

--- client/src/screens/about/AboutMe.jsx
+++ client/src/screens/about/AboutMe.jsx
@@ -5,12 +5,13 @@ import sarah2 from "../../assets/pictures/sarah-on-rock.jpeg";
 export default function AboutMe() {
   return (
     <Layout>
-      <div className="w-screen">
+      <div className="w-screen flex flex-row items-center ">
         <img
-          className="h-96 m-4"
+          className="h-96 m-4 rounded-md"
           src={sarah}
           alt="a selfie of a woman who is the artist"
         />
+        <h2 className="m-48 font-IBM-Plex-Serif text-3xl">About Me</h2>
       </div>
       <div className="text-left m-4">
         <p>
@@ -31,7 +32,7 @@ export default function AboutMe() {
       </div>
       <div className="w-screen flex flex-row justify-end">
         <img
-          className="h-96 m-4"
+          className="h-96 m-4 rounded-md"
           src={sarah2}
           alt="the artist of the website standing on a rock with her arms out"
         />
