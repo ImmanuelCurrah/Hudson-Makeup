a984bc8e001a541d44e31aecce80d172df757190
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-25 10:18:11 -0400

    added the routing to bridal

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import AboutMe from "./screens/about/AboutMe";
 import Booking from "./screens/booking/Booking";
+import Bridal from "./screens/bridal/Bridal";
 import Home from "./screens/home/Home";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about%20me" element={<AboutMe />} />
         <Route path="/booking" element={<Booking />} />
+        <Route path="/bridal" element={<Bridal />} />
       </Routes>
     </div>
   );
