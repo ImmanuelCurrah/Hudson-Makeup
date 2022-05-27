dcc0a980bcc153f9882c876850e667bede2c633a
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-27 13:41:50 -0400

    added in the covid policy

--- client/src/components/bridal-components/bridal-covid-policy/BridalCovidCard.jsx
+++ client/src/components/bridal-components/bridal-covid-policy/BridalCovidCard.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function BridalCovidCard() {
-  return <div className="bg-white w-screen">BridalCovidCard</div>;
+  return (
+    <div className="bg-white w-screen">
+      <div className="m-4 font-IBM-Plex-Serif text-xl">COVID Policy</div>
+      <p className="m-4 font-IBM-Plex-Serif font-bold">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo, aliquam
+        ut natus perspiciatis incidunt iure. Veritatis officiis atque minus
+        sapiente eligendi fuga tempora porro, qui quod ea dolores itaque sequi.
+      </p>
+    </div>
+  );
 }
