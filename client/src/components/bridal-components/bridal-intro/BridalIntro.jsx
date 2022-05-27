1bf851554c8e2dbf63745f86b76fff5106edba70
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-27 10:38:00 -0400

    birdal mission is donish

--- client/src/components/bridal-components/bridal-intro/BridalIntro.jsx
+++ client/src/components/bridal-components/bridal-intro/BridalIntro.jsx
@@ -2,7 +2,7 @@ import bride from "../../../assets/bridal-filler-pics/bridal-top.png";
 
 export default function BridalIntro() {
   return (
-    <div className="flex flex-row m-4">
+    <div className="flex flex-row m-4 items-center justify-evenly">
       <img
         className="h-52 rounded-md"
         src={bride}
@@ -13,10 +13,12 @@ export default function BridalIntro() {
           Some catchy little phrase
         </h2>
         <p className="text-left font-IBM-Plex-Serif font-semibold">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-          numquam porro officia natus. Quas, laborum facilis optio dignissimos
-          dolor doloribus quos, fugit exercitationem aliquam voluptatibus
-          consequatur blanditiis! Harum, veritatis.
+          The bridal makeup I do is more natural looking, bringing our your
+          beauty to shine as big as the love in your heart. Like the look, all
+          the products I will be using are natural as well. I only use clean,
+          non toxic, high quality makeup to nurture your health as well as your
+          beauty. I will make the makeup process a fun, stress free one. Leave
+          all the planning up to me! Can’t wait to be your supporter.
         </p>
       </div>
     </div>
--- client/src/components/bridal-components/bridal-mission/BridalMission.jsx
+++ client/src/components/bridal-components/bridal-mission/BridalMission.jsx
@@ -1,5 +1,26 @@
-import React from "react";
+import bride from "../../../assets/bridal-filler-pics/bridal-mission.png";
 
 export default function BridalMission() {
-  return <div>BridalMission</div>;
+  return (
+    <div className="bg-white w-screen flex flex-row justify-evenly items-center">
+      <div className="m-4">
+        <div>My mission is to help you...</div>
+        <div>
+          <ul>
+            <li>Break out</li>
+            <li>feel your best</li>
+            <li>be beautiful</li>
+            <li>shine</li>
+            <li>relax</li>
+            <li>Have fun</li>
+          </ul>
+        </div>
+      </div>
+      <img
+        className="w-96 m-4 rounded-md"
+        src={bride}
+        alt="a bride and her spouse on a rocky outcrop"
+      />
+    </div>
+  );
 }
