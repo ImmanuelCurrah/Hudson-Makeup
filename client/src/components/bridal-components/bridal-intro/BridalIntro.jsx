11d6f02ddf321dbcd2034a710f8b86a33b129829
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-27 10:20:52 -0400

    bridal intro is donish

--- client/src/components/bridal-components/bridal-intro/BridalIntro.jsx
+++ client/src/components/bridal-components/bridal-intro/BridalIntro.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import bride from "../../../assets/bridal-filler-pics/bridal-top.png";
 
 export default function BridalIntro() {
-  return <div>BridalIntro</div>;
+  return (
+    <div className="flex flex-row m-4">
+      <img
+        className="h-52 rounded-md"
+        src={bride}
+        alt="a bride holding flowers"
+      />
+      <div className="m-4">
+        <h2 className="m-2">Some catchy little phrase</h2>
+        <p className="text-left">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          numquam porro officia natus. Quas, laborum facilis optio dignissimos
+          dolor doloribus quos, fugit exercitationem aliquam voluptatibus
+          consequatur blanditiis! Harum, veritatis.
+        </p>
+      </div>
+    </div>
+  );
 }
--- client/src/screens/bridal/Bridal.jsx
+++ client/src/screens/bridal/Bridal.jsx
@@ -1,9 +1,23 @@
+import BridalCovidCard from "../../components/bridal-components/bridal-covid-policy/BridalCovidCard";
+import BridalFAQ from "../../components/bridal-components/bridal-faq/BridalFAQ";
+import BridalIntro from "../../components/bridal-components/bridal-intro/BridalIntro";
+import BridalMission from "../../components/bridal-components/bridal-mission/BridalMission";
+import BridalProcess from "../../components/bridal-components/bridal-process/BridalProcess";
+import BridalQuoteCard from "../../components/bridal-components/bridal-quote-box/BridalQuoteCard";
 import Layout from "../../components/layout/Layout";
 
 export default function Bridal(props) {
   return (
     <Layout handleDisplay={props.handleDisplay}>
-      <div>Bridal</div>
+      <div className="font-IBM-Plex-Serif text-4xl m-4">
+        All Natural Beauty for the Bridal Party
+      </div>
+      <BridalIntro />
+      <BridalMission />
+      <BridalProcess />
+      <BridalFAQ />
+      <BridalCovidCard />
+      <BridalQuoteCard />
     </Layout>
   );
 }
