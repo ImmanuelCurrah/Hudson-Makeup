34b2a63595e6a13f1d5cd8de3dfb7ad337409a16
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-31 13:58:54 -0400

    starting to create the booking form

--- client/src/screens/booking/Booking.jsx
+++ client/src/screens/booking/Booking.jsx
@@ -1,12 +1,14 @@
+import BookingForm from "../../components/booking-form/BookingForm";
 import Layout from "../../components/layout/Layout";
 
 export default function Booking(props) {
   return (
     <Layout handleDisplay={props.handleDisplay}>
-      <div>
-        "I am here to make your bridal makeup experience as stress free as
+      <div className="m-4 font-IBM-Plex-Serif text-2xl">
+        "My mission is to make your bridal makeup experience as stress free as
         possible!"
       </div>
+      <BookingForm />
     </Layout>
   );
 }
