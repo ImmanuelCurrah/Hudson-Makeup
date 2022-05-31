b2f8be76d07398bd0ec87b7a98fa9b8bf178dab1
Immanuel Currah <immanueli.currah@gmail.com>
2022-05-31 13:56:23 -0400

    starting booking screen

--- client/src/screens/booking/Booking.jsx
+++ client/src/screens/booking/Booking.jsx
@@ -3,7 +3,10 @@ import Layout from "../../components/layout/Layout";
 export default function Booking(props) {
   return (
     <Layout handleDisplay={props.handleDisplay}>
-      <div>Booking</div>
+      <div>
+        "I am here to make your bridal makeup experience as stress free as
+        possible!"
+      </div>
     </Layout>
   );
 }
