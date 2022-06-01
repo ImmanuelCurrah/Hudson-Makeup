79111f494427114832e195f51927637c829ae54f
Immanuel Currah <immanueli.currah@gmail.com>
2022-06-01 15:56:30 -0400

    filling out the faq

--- client/src/components/bridal-components/bridal-faq/BridalFAQ.jsx
+++ client/src/components/bridal-components/bridal-faq/BridalFAQ.jsx
@@ -9,37 +9,62 @@ export default function BridalFAQ() {
       <div className="flex flex-col items-center justify-center"></div>
       <div className="grid grid-cols-2 gap-16 m-4 phone:flex flex-col items-center">
         <div>
-          <h3 className="font-IBM-Plex-Serif">Here is a test question</h3>
-          <p className="font-IBM-Plex-Serif font-semibold">answer</p>
-        </div>
-        <div>
-          <h3 className="font-IBM-Plex-Serif">Question</h3>
+          <h3 className="font-IBM-Plex-Serif">
+            What is the biggest party you can do?
+          </h3>
           <p className="font-IBM-Plex-Serif font-semibold">
-            Here is another long drawn out sample text because I am super sick
-            of using lorem crap over and over and I want to see something that
-            makes sense in someway on the page
+            The max group I can do is around six people. This is becausee I am
+            working by myself for the time being. I am committed to making sure
+            that I can take care of each person and giving them the time they
+            deserve to feel beautiful.
           </p>
         </div>
         <div>
           <h3 className="font-IBM-Plex-Serif">
-            Here is a longer test question for length
+            Where and when do trials happen?
           </h3>
-          <p className="font-IBM-Plex-Serif font-semibold">answer</p>
-        </div>
-        <div>
-          <h3 className="font-IBM-Plex-Serif">Question</h3>
-          <p className="font-IBM-Plex-Serif font-semibold">answer</p>
-        </div>
-        <div>
-          <h3 className="font-IBM-Plex-Serif">Question</h3>
           <p className="font-IBM-Plex-Serif font-semibold">
-            Here is a longer test answer for length to get an idea of what a
-            real answer to a real question would look like on the page
+            Trials will take place about one month before the event. The trial
+            location will be decided during the booking phase. If the
+            destination decided upon is far away, a travel fee may apply.
           </p>
         </div>
         <div>
-          <h3 className="font-IBM-Plex-Serif">Question</h3>
-          <p className="font-IBM-Plex-Serif font-semibold">answer</p>
+          <h3 className="font-IBM-Plex-Serif">
+            How should I prepare for the trial and day of?
+          </h3>
+          <p className="font-IBM-Plex-Serif font-semibold">
+            Just be yourself! I am here to make your makeup journey as stress
+            free and relaxing as possible. However that looks for you, I can
+            make it happen. This is about you!
+          </p>
+        </div>
+        <div>
+          <h3 className="font-IBM-Plex-Serif">Do you travel?</h3>
+          <p className="font-IBM-Plex-Serif font-semibold">
+            I am based in the Hudson Valley. Travel is something I am open too.
+            There is a travel fee that is outlined.
+          </p>
+        </div>
+        <div>
+          <h3 className="font-IBM-Plex-Serif">
+            Do you use only clean and natural products?
+          </h3>
+          <p className="font-IBM-Plex-Serif font-semibold">
+            Yes! I am extremely committed to staying as natural as possible.
+            There are no synthetic makeups in my kit. Everything is ethically
+            and naturally sourced.
+          </p>
+        </div>
+        <div>
+          <h3 className="font-IBM-Plex-Serif">
+            Can I add more people to the contract?
+          </h3>
+          <p className="font-IBM-Plex-Serif font-semibold">
+            Currently I can only do small parties. But if you want to add more
+            people to your party please reach out and we can discuss details
+            further!
+          </p>
         </div>
       </div>
       <div className="flex flex-col items-center justify-center">
