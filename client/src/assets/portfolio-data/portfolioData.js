930a5d45135e7ea854682d19458163f19585e8d6
Immanuel Currah <immanueli.currah@gmail.com>
2022-06-06 09:18:07 -0400

    adding pictures to the portfolio

--- client/src/assets/portfolio-data/portfolioData.js
+++ client/src/assets/portfolio-data/portfolioData.js
@@ -1,1 +1,19 @@
-export const pictures = [];
+import port1 from "../pictures/1.jpeg";
+import port2 from "../pictures/2.jpeg";
+import port3 from "../pictures/3.jpeg";
+import port4 from "../pictures/4.jpeg";
+import port5 from "../pictures/5.jpeg";
+import port6 from "../pictures/6.jpg";
+import port7 from "../pictures/7.jpeg";
+import port8 from "../pictures/8.jpeg";
+
+export const pictures = [
+  { picture: port5, description: "a brides maid" },
+  { picture: port4, description: "a brides maid" },
+  { picture: port7, description: "a brides maid" },
+  { picture: port3, description: "a brides maid" },
+  { picture: port8, description: "a brides maid" },
+  { picture: port2, description: "a brides maid" },
+  { picture: port6, description: "a brides maid" },
+  { picture: port1, description: "a brides maid" },
+];
