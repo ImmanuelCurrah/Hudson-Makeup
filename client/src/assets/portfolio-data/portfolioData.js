575919600f69731438542c27a4e0d2b435843317
Immanuel Currah <immanueli.currah@gmail.com>
2022-06-28 15:30:55 -0400

    added pictures to the portfolio

--- client/src/assets/portfolio-data/portfolioData.js
+++ client/src/assets/portfolio-data/portfolioData.js
@@ -3,17 +3,23 @@ import port2 from "../pictures/2.jpeg";
 import port3 from "../pictures/3.jpeg";
 import port4 from "../pictures/4.jpeg";
 import port5 from "../pictures/5.jpeg";
-import port6 from "../pictures/6.jpg";
 import port7 from "../pictures/7.jpeg";
-import port8 from "../pictures/8.jpeg";
+import port9 from "../pictures/firstSet.jpeg";
+import port10 from "../pictures/firstSet1.jpeg";
+import port11 from "../pictures/firstSet2.jpeg";
+import port12 from "../pictures/firstSet3.jpeg";
+import port13 from "../pictures/firstSet4.jpeg";
 
 export const pictures = [
   { picture: port5, description: "a brides maid" },
   { picture: port4, description: "a brides maid" },
   { picture: port7, description: "a brides maid" },
   { picture: port3, description: "a brides maid" },
-  { picture: port8, description: "a brides maid" },
   { picture: port2, description: "a brides maid" },
-  { picture: port6, description: "a brides maid" },
   { picture: port1, description: "a brides maid" },
+  { picture: port9, description: "a brides maid" },
+  { picture: port10, description: "a brides maid" },
+  { picture: port11, description: "a brides maid" },
+  { picture: port12, description: "a brides maid" },
+  { picture: port13, description: "a brides maid" },
 ];
